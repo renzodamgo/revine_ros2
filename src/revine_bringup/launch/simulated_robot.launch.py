acc7309a138bd1c606f2bcbb1189d0de8bd20996
Renzo Damian <renzo.damian.go@gmail.com>
2024-11-12 05:04:02 +0000

    Add robot stl to gazebo sim

---
+++ src/revine_bringup/launch/simulated_robot.launch.py
@@ -0,0 +1,45 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    gazebo = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("revine_description"),
+            "launch",
+            "gazebo.launch.py",
+        ),
+    )
+
+    # controller = IncludeLaunchDescription(
+    #     os.path.join(
+    #         get_package_share_directory("revine_controller"),
+    #         "launch",
+    #         "controller.launch.py"
+    #     ),
+    #     launch_arguments={
+    #         "use_simple_controller": "False",
+    #         "use_python": "False"
+    #     }.items(),
+    # )
+
+    # joystick = IncludeLaunchDescription(
+    #     os.path.join(
+    #         get_package_share_directory("revine_controller"),
+    #         "launch",
+    #         "joystick_teleop.launch.py"
+    #     ),
+    #     launch_arguments={
+    #         "use_sim_time": "True"
+    #     }.items()
+    # )
+
+    return LaunchDescription(
+        [
+            gazebo,
+            # controller,
+            # joystick,
+        ]
+    )
